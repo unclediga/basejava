3bda5a65ebe63bb1ee46f26c9cc1cf33e4e5cdbb
unclediga <zowie_i@mail.ru>
2019-07-15 14:29:14 +0300

    HW1-with-cycles

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,60 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int curSize = 0;
+
 
     void clear() {
+        for (int i = 0; i < curSize; i++) {
+            storage[i] = null;
+        }
+        curSize = 0;
     }
 
     void save(Resume r) {
+        storage[curSize++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < curSize; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int i = 0;
+        boolean flag = false; // true = элемент найден  и удалён
+        // найти и удалить
+        while( i < curSize){
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                flag = true;
+                break;
+            }
+            i++;
+        }
+        // сместить хвост, закрыв null
+        while (i < curSize-1){
+            storage[i] = storage[i+1];
+            i++;
+        }
+        if (flag) curSize--;
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, curSize);
     }
 
     int size() {
-        return 0;
+        return curSize;
     }
 }
