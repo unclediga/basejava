a724e75e4520b4d00cb33f3f851e4dda10bcfc2e
unclediga <zowie_i@mail.ru>
2019-07-22 18:21:25 +0300

    HW2-fix-mistakes-1

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -10,51 +10,47 @@ import java.util.Arrays;
 public class ArrayStorage {
 
     // Для дальнейших проверок на переполнение выносим размер массива в константу.
-    final int STORAGE_SIZE = 10000;
+    final static private int STORAGE_SIZE = 10_000;
     private Resume[] storage = new Resume[STORAGE_SIZE];
     private int size = 0;
 
     public void clear() {
         // Используем подходящий метод из java.util.Arrays
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
-
     }
 
-    public void save(Resume r) {
-        int i = getIdx(r.getUuid());
-        if (i != -1) {
+    public void save(Resume resume) {
+        int idx = getIdx(resume.getUuid());
+        if (idx != -1) {
             System.out.println("Resume present in Storage!");
             return;
-        } else if (size + 1 > STORAGE_SIZE) {
+        } else if (size >= STORAGE_SIZE) {
             System.out.println("Storage overflow!");
             return;
         }
-        storage[size] = r;
+        storage[size] = resume;
         size++;
-
     }
 
     public Resume get(String uuid) {
-        int i = getIdx(uuid);
-        if (i == -1) {
+        int idx = getIdx(uuid);
+        if (idx == -1) {
             System.out.println("Resume not present in Storage!");
             return null;
         }
-        return storage[i];
-
+        return storage[idx];
     }
 
     public void delete(String uuid) {
-        int i = getIdx(uuid);
-        if (i == -1) {
+        int idx = getIdx(uuid);
+        if (idx == -1) {
             System.out.println("Resume not present in Storage!");
             return;
         }
-        storage[i] = storage[size - 1];
-        storage[size - 1] = null;
         size--;
-
+        storage[idx] = storage[size];
+        storage[size] = null;
     }
 
     /**
@@ -63,9 +59,7 @@ public class ArrayStorage {
 
     public Resume[] getAll() {
         // Используем подходящий метод из java.util.Arrays
-        Resume[] result = Arrays.copyOf(storage, size);
-        return result;
-
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
