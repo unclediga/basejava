0d1be5c46eb1efa49904e5ff29c0b3bb4ae9019e
unclediga <zowie_i@mail.ru>
2019-12-17 18:38:49 +0300

    HW12-part2

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -1,32 +1,70 @@
 package ru.javawebinar.basejava;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+import java.util.Random;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 
-import static java.util.Arrays.asList;
 import static java.util.Arrays.stream;
 
 public class MainStreams {
     public static void main(String[] args) {
         MainStreams mainStreams = new MainStreams();
-//        System.out.println(mainStreams.minValue(new int[]{1, 2, 3, 3, 2, 3}));
-//        System.out.println(mainStreams.minValue(new int[]{9, 8, 7}));
-//        System.out.println(mainStreams.oddOrEven(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7)));
-        mainStreams.oddOrEven(asList(0, 1, 2, 3, 4, 5, 6, 7));
+        System.out.println(mainStreams.getMinNumber(new int[]{1, 2, 3, 3, 2, 3}));
+        System.out.println(mainStreams.getMinNumber(new int[]{9, 8, 7}));
+
+        final int SIZE = 1000;
+        int sum = 0;
+        Random rnd = new Random();
+        ArrayList<Integer> list = new ArrayList<>(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            int num = rnd.nextInt(SIZE);
+            list.add(num);
+            sum += num;
+        }
+        System.out.printf("sum =%d : %s   \n", sum, list.subList(0, Math.min(list.size(), 20)));
+        List<Integer> list2 = mainStreams.getOddOrEven(list);
+        System.out.println(list2.subList(0, Math.min(list2.size(), 20)));
     }
 
-    int minValue(int[] values) {
-        return stream(values).distinct().sorted().reduce(0, (acc, x) -> acc * 10 + x);
+    int getMinNumber(int[] values) {
+        return stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (acc, x) -> acc * 10 + x);
     }
 
+    List<Integer> getOddOrEven(List<Integer> list) {
+        Supplier<Combi> supplier = Combi::new;
+        BiConsumer<Combi, Integer> accumulator = (acc, x) -> {
+            if (x % 2 == 0) {
+                acc.ev.add(x);
+            } else {
+                acc.od.add(x);
+            }
+            acc.sum += x;
+        };
+        BinaryOperator<Combi> combiner = (left, right) -> {
+            left.ev.addAll(right.ev);
+            left.od.addAll(right.od);
+            left.sum += right.sum;
+            return left;
+        };
+        Function<Combi, List<Integer>> finisher = c -> c.sum % 2 == 0 ? c.ev : c.od;
 
-    List<Integer> oddOrEven(List<Integer> integers) {
-        List<Integer> list = integers;
-        AtomicInteger cnt = new AtomicInteger(0);
-//        List<Integer> collect = list.stream().peek(x -> cnt.set(cnt.incrementAndGet())).filter(x -> {if (cnt.get() % 2 == 0) return true; else return false;}).collect(Collectors.toList());
-        List<Integer> collect = list.stream().filter(x -> (list.stream().reduce(0, (acc, y) -> acc + y)) == 3).collect(Collectors.toList());
-        System.out.println("coll = " + collect);
-        return list;
+        return list
+                .stream()
+                .collect(Collector.of(supplier, accumulator, combiner, finisher));
+    }
+
+    static class Combi {
+        int sum = 0;
+        ArrayList<Integer> ev = new ArrayList<>();
+        ArrayList<Integer> od = new ArrayList<>();
     }
 }
+
