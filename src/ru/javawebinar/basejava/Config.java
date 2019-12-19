b80cedc7ff606ad7846b327946154d9b0888dfea
unclediga <zowie_i@mail.ru>
2019-12-19 18:11:37 +0300

    HW13

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -7,10 +7,13 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static Config INSTANCE = new Config();
     private static File PROPS = new File("config\\resumes.properties");
+    private static Config INSTANCE = new Config();
     private Properties props = new Properties();
     private File storageDir;
+    private final String dbUrl;
+    private final String dbUser;
+    private final String dbPassword;
 
     public static Config get() {
         return INSTANCE;
@@ -20,6 +23,9 @@ public class Config {
         try (InputStream is = new FileInputStream(PROPS)) {
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
+            dbUrl = props.getProperty("db.url");
+            dbUser = props.getProperty("db.user");
+            dbPassword = props.getProperty("db.password");
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
@@ -27,5 +33,17 @@ public class Config {
 
     public File getStorageDir() {
         return storageDir;
+    }
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getDbUser() {
+        return dbUser;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
     }
 }
--- src/ru/javawebinar/basejava/storage/SQLStorage.java
+++ src/ru/javawebinar/basejava/storage/SQLStorage.java
@@ -1,75 +1,141 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.sql.ConnectionFactory;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLStorage implements Storage {
-    private final ConnectionFactory connectionFactory;
+    private final SQLHelper helper;
 
     SQLStorage(String dbUrl, String dbUser, String dbPassword) {
-        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        helper = new SQLHelper(dbUrl, dbUser, dbPassword);
     }
 
     @Override
     public void clear() {
-        try (Connection conn = connectionFactory.getConnection()) {
+        helper.execute(conn -> {
             PreparedStatement ps = conn.prepareStatement("DELETE FROM resume");
             ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException("Error connection", e);
-        }
+        });
     }
 
     @Override
     public void update(Resume resume) {
-
+        helper.execute(conn -> {
+            PreparedStatement ps = conn.prepareStatement("UPDATE resume SET full_name = ? WHERE uuid = ?");
+            ps.setString(1, resume.getFullName());
+            ps.setString(2, resume.getUuid());
+            if (ps.executeUpdate() == 0)
+                throw new NotExistStorageException(resume.getUuid());
+        });
     }
 
     @Override
     public void save(Resume resume) {
-        try (Connection conn = connectionFactory.getConnection()) {
-            PreparedStatement ps = conn.prepareStatement("INSERT resume(uuid, full_name) VALUES(?,?)");
+        helper.execute(conn -> {
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO resume(uuid, full_name) VALUES(?,?)");
             ps.setString(1, resume.getUuid());
             ps.setString(2, resume.getFullName());
-            ps.execute();
-        } catch (SQLException e) {
-            throw new StorageException("Error connection", e);
-        }
+            try {
+                ps.execute();
+            } catch (SQLException e) {
+//                The SQLState is a 5-char code, of which the first two are common among all DB's...
+//                23: integrity constraint violation
+//                ...
+                if (e.getSQLState().startsWith("23")) {
+                    throw new ExistStorageException(resume.getUuid());
+                } else {
+                    throw e;
+                }
+            }
+        });
     }
 
     @Override
     public Resume get(String uuid) {
-        try (Connection conn = connectionFactory.getConnection()) {
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid=?");
+        return helper.executeAndGet(conn -> {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r WHERE r.uuid = ?");
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
-            Resume resume = new Resume(uuid, rs.getString("full_name"));
-            return resume;
-        } catch (SQLException e) {
-            throw new StorageException("Error connection", e);
-        }
+            return helper.getResume(rs);
+        });
     }
 
     @Override
     public void delete(String uuid) {
-
+        helper.execute(conn -> {
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM resume WHERE uuid = ?");
+            ps.setString(1, uuid);
+            if (ps.executeUpdate() == 0)
+                throw new NotExistStorageException(uuid);
+        });
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        return null;
+        ArrayList<Resume> list = new ArrayList<>();
+        helper.execute(conn -> {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r ORDER BY full_name");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(helper.getResume(rs));
+            }
+        });
+        return list;
     }
 
     @Override
     public int size() {
-        return 0;
+        return helper.executeAndGet(conn -> {
+            PreparedStatement ps = conn.prepareStatement("SELECT count(*) FROM resume");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        });
+    }
+
+    static class SQLHelper {
+        private final ConnectionFactory connectionFactory;
+
+        public SQLHelper(String dbUrl, String dbUser, String dbPassword) {
+            connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        }
+
+        public <T> void execute(codeNoValue<T> codeBlock) {
+            try (Connection conn = connectionFactory.getConnection()) {
+                codeBlock.exec(conn);
+            } catch (SQLException e) {
+                throw new StorageException("SQL Error", e);
+            }
+        }
+
+        public <T> T executeAndGet(codeReturnValue<T> codeBlock) {
+            try (Connection conn = connectionFactory.getConnection()) {
+                return codeBlock.exec(conn);
+            } catch (SQLException e) {
+                throw new StorageException("SQL Error", e);
+            }
+        }
+
+        public Resume getResume(ResultSet rs) throws SQLException {
+            return new Resume(rs.getString(1).trim(), rs.getString("full_name"));
+        }
+    }
+
+    interface codeReturnValue<T> {
+        T exec(Connection conn) throws SQLException;
+    }
+
+    interface codeNoValue<T> {
+        void exec(Connection conn) throws SQLException;
     }
 }
