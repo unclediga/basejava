491ee64d4fb0f00f0970f1e6a65541502b252885
unclediga <zowie_i@mail.ru>
2020-01-16 16:32:34 +0300

    Lesson16 JSP

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.basejava.web;
 
 import ru.javawebinar.basejava.Config;
-import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
@@ -27,27 +25,7 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        PrintWriter writer = response.getWriter();
-        writer.write("<html>");
-        writer.write("<body>");
-        writer.write("<table border=\"1\" cellpadding=\"20\">");
-        writer.write("<tr>");
-        writer.write("<th>UUID</td>");
-        writer.write("<th>Full Name</td>");
-        writer.write("</tr>");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write("<tr>");
-            writer.write("<td>" + resume.getUuid() + "</td><td>" + resume.getFullName() + "</td>");
-            writer.write("</tr>");
-        }
-        writer.write("</table>");
-        writer.write("</body>");
-        writer.write("</html>");
-        writer.flush();
-        writer.close();
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 }
